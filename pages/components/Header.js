39bfa3f6e8fff0873c51a122ccf41bb1752efddf
Ozyma <ozymacode@gmail.com>
2022-08-28 01:40:21 -0700

    commit

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -95,7 +95,7 @@ function DrawerAppBar(props) {
                 component="div"
                 sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
               >
-                Welcome!
+                Welcome! a
               </Typography>
             </Grid>
             <Grid
